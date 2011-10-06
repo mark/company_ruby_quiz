14b438814fe7be9a5dd4d0eb27b4a3e9ae6f3c9e
Mark Josef <mcphage@gmail.com>
2011-10-06 14:55:05 -0400

    Added clearer tests for Nonary Game problem.

--- problem_3/nonary_game_test.rb
+++ problem_3/nonary_game_test.rb
@@ -5,10 +5,23 @@ class NonaryGameTest < Test::Unit::TestCase
 
   SOLUTIONS = %w( 12357 12456 135 1458 15678 2358 2457 3456 34578 567 )
   
-  def test_solutions_to_nonary_game
+  def setup
     nonary_game = NonaryGame.new
-    solutions = nonary_game.valid_groups
-    sorted_solutions = solutions.map { |sol| sol.sort.join }.sort
+    @solutions = nonary_game.valid_groups
+  end
+  
+  def test_returns_an_array_of_solutions
+    assert_kind_of Array, @solutions
+  end
+  
+  def test_each_solution_is_an_array
+    @solutions.each do |solution|
+      assert_kind_of Array, solution
+    end
+  end
+  
+  def test_solutions_to_nonary_game
+    sorted_solutions = @solutions.map { |sol| sol.sort.join }.sort
 
     assert_equal SOLUTIONS, sorted_solutions
   end
