29cb121d09748b870daf02eab369a6a806054be0
Kyle Stevens <kstevens715@gmail.com>
2013-08-12 08:46:32 -0400

    Use Ruby's built in library

--- problem_2/country_ip_test.rb
+++ problem_2/country_ip_test.rb
@@ -1,4 +1,5 @@
 require 'test/unit'
+require 'digest'
 require './country_ip'
 
 class CountryIpTest < Test::Unit::TestCase
@@ -24,12 +25,7 @@ class CountryIpTest < Test::Unit::TestCase
   end
 
   def test_csv_file_not_changed
-    checksum = '22620fdd50ebaef84dd3d9521beb6a7c'
-    if which('md5')
-      assert_equal checksum, `md5 -q IpToCountry.csv`.strip
-    else
-      assert_equal checksum, `md5sum IpToCountry.csv`.split(' ')[0].strip
-    end
+    assert_equal '22620fdd50ebaef84dd3d9521beb6a7c', Digest::MD5.hexdigest(File.read('IpToCountry.csv'))
   end
   
   def test_67_99_163_76_is_United_States
@@ -42,18 +38,6 @@ class CountryIpTest < Test::Unit::TestCase
 
   def test_200_100_100_100_is_Brazil
     assert_equal "Brazil", @country_ip.search("200.100.100.100")
-  end
-
-  # Thanks to: http://stackoverflow.com/a/5471032
-  def which(cmd)
-    exts = ENV['PATHEXT'] ? ENV['PATHEXT'].split(';') : ['']
-    ENV['PATH'].split(File::PATH_SEPARATOR).each do |path|
-      exts.each { |ext|
-        exe = File.join(path, "#{cmd}#{ext}")
-        return exe if File.executable? exe
-      }
-    end
-    return nil
   end
 
 end
