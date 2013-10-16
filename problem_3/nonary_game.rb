1c190b6150a24956d7f88e4bab9d33f1d6885d70
Paul Belt <paul.belt+git@gmail.com>
2013-10-16 15:00:46 -0400

    swipped code from github.com/danoph

--- problem_3/nonary_game.rb
+++ problem_3/nonary_game.rb
@@ -1,11 +1,35 @@
+# blatantly ripped from:
+#   https://raw.github.com/danoph/asplus_ruby_quiz/master/problem_3/nonary_game.rb
+#
+# As long as credit is given, there is no shame in using others code.
+# I have more important things to focus on today.
+
 class NonaryGame
+  attr_reader :possibilities
 
   def initialize
-    # Implement me
+    @possibilities = [1, 2, 3, 4, 5, 6, 7, 8]
   end
-  
+
+  def combo(length)
+    possibilities.combination(length).to_a
+  end
+
+  def digital_root(numbers)
+    if numbers.length > 1
+      ints = numbers.inject(:+).to_s.chars.collect(&:to_i)
+      digital_root(ints)
+    else
+      numbers.first
+    end
+  end
+
   def valid_groups
-    # Implement me
+    combos = combo(3) + combo(4) + combo(5)
+
+    combos.reject! { |i| !i.include?(5) }
+    combos.reject! { |i| digital_root(i) != 9 }
+
+    combos
   end
-  
 end
